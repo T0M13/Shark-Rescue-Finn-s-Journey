52cd6f67333821e9757ad71cfc637533a1d53c82
BrunnerWolfgang <WolfiundBella@gmail.com>
2023-07-18 16:25:42 +0200

    Added Simple MainMenu

---
+++ SharkRescue_FinsJourney/Assets/Project/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json.Bson;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Prototype");
+    }
+}
